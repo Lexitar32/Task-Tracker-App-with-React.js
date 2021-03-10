5f3ad23bf2ca1bd2c19d37d20b4354678105af06
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2021-03-10 16:50:37 +0100

    Done with the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-// Importing Components
+// Importing Components & Packages
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import { useState } from 'react';
+import AddTask from './components/AddTask';
 
 function App() {
+    const [showAddTask, setShowAddTask] = useState(false);
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -25,12 +27,33 @@ function App() {
         }
     ]
     )
+
+    // Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1;
+
+        const newTask = { id, ...task}
+
+        setTasks([...tasks, newTask]);
+    }
+    
+     // Toggle Reminder
+     const toggleReminder = (id) => {
+         setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+     }
+
+    // Delete Task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    }
+
     return (
         <div className="container">
-            <Header />
-            <Tasks tasks={tasks} />
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Task Found!')}
         </div>
     )
 }
 
-export default App;
+export default App
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,9 @@
 import Task from './Task';
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
-            {tasks.map((task) => (<Task key={task.id} task={task} />))}
+            {tasks.map((task) => (<Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />))}
         </>
     )
 }
