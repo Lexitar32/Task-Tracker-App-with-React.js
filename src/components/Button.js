c17ea8d222d0cb92214230ea45428f71553a649f
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2021-06-14 02:21:53 +0100

    :sparkles: All App Functionalities Done!

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,5 @@
-import PropTypes from 'prop-types';
-
 const Button = ({ color, text, onClick }) => {
-    return <button onClick={onClick} style={{ backgroundColor:color }} className="btn">{text}</button>
-}
-
-Button.defaultProps = {
-    color: 'steelBlue'
-}
-
-Button.propTypes = {
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
+    return <button onClick={onClick} style={{ backgroundColor: color }} className="btn">{text}</button>
 }
 
 export default Button
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,21 @@
-import { FaTimes } from 'react-icons/fa';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import "../index.css"
 
-const Task = ({ task, onDelete, onToggle }) => {
+const Task = ({ task, onDelete, onEdit }) => {
     return (
-        <div className={`task ${task.reminder ? 'reminder' : ''} `} onDoubleClick={() => {onToggle(task.id)}}>
-            <h3>
-                {task.text} <FaTimes onClick={() => onDelete(task.id)} style={{color: 'red', cursor: 'pointer'}} />
-            </h3>
-            <p>{task.day}</p>
+        <div>
+            <div className="task">
+                <div>
+                    <p className="taskName">
+                        <span className="textBold">Task Name:</span> {task.text}
+                    </p>
+                    <p className="taskDate"><span className="textBold">Date of Completion:</span> {task.day}</p>
+                </div>
+                <div>
+                    <p><FaTimes onClick={() => onDelete(task.id)} className="delIcon" /></p>
+                    <p><FaPencilAlt onClick={() => onEdit(task.id)} className="editIcon" /></p>
+                </div>
+            </div>
         </div>
     )
 }
